91888ef750ad46c53387cdfc4171b7f77e85dd50
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-04-13 16:16:14 +0530

    Add files via upload

---
+++ range_methods_part-1.py
@@ -0,0 +1,70 @@
+# a=1,2,3,4,5
+# print(*a)
+
+# a=range(10)
+# print(a)
+
+# a=10,20,30
+# for j in a:
+    # print(j,end=' ')
+
+# for i in range(7):
+#     print(i)
+
+# n=int(input("Enter the value of n:"))
+# for i in range (1,n+1):
+#     print(i,end="\n")
+
+# for i in range(1,30,2):
+#     print(i)
+
+# for i in range(2,30,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(10,-1,-1):  
+#     print(i)
+
+# n=[1,3.5,'akhila']
+# for i in n[::-1]:
+#     print(i)
+
+# v='programming'
+# for e in v[::-2]:
+#     print(e)
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(n):
+#     print(i)
+#     print(i)
+#     print(i)
+#     print("The Loop has been executed.")
+# print("done")
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(2,n):
+#     print(i)
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(20,n):
+#     print(i)
+
+# n=int(input("Enter any Integer Number:"))
+# for i in range(2,n,2):
+#     print(I)
+
+# n=int(input("Enter any Integer Number:"))
+# if(n%2==0):
+#     print(f"{n} Even Number")
+# else:
+#     print(f"{n} Odd Number")
+
+# for i in range(1,5):
+#     print(i*i*i)
+
+n=int(input("Enter the value of n:"))
+while(n<8):
+    print("hello",str(n))
+    n+=1
